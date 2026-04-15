3949162e511565d445e48b2ce36055615f18e040
tittymaps <info@tittymaps.com>
2026-04-15 17:31:13 -0400

    Update page.tsx

--- app/dancers/[id]/page.tsx
+++ app/dancers/[id]/page.tsx
@@ -36,7 +36,7 @@ export default function DancerProfile() {
     </div>
   )
 
-  const fanslyUrl = dancer.fansly_url ? `${dancer.fansly_url}?ref=${FANSLY_REF}` : ''
+  const fanslyUrl = dancer.fansly_url ? `${dancer.fansly_url}?r=${FANSLY_REF}` : ''
   const allPhotos: string[] = dancer.photo_urls || (dancer.photo_url ? [dancer.photo_url] : [])
 
   return (
