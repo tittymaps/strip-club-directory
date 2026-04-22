6f66c9da33fffde1033032920415511c232eeb2f
tittymaps <info@tittymaps.com>
2026-04-22 17:42:38 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -263,7 +263,7 @@ export default function Home() {
     { key: 'bikini', label: '👙 Bikini' },
     { key: 'full_bar', label: '🍾 Full bar' },
     { key: 'byob', label: '🍺 BYOB' },
-    { key: 'cafe', '🧋 Cafe' },
+    { key: 'cafe', label: '🧋 Cafe' },
   ]
 
   return (
