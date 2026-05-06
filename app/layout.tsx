156616a47a401ec9d97481222b319923de4f1c91
tittymaps <info@tittymaps.com>
2026-05-06 11:16:56 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,14 +22,13 @@ export const metadata: Metadata = {
   },
   icons: {
     icon: [
-      { url: '/favicon-96x96.png', sizes: '96x96', type: 'image/png' },
+      { url: '/favicon.ico' },
       { url: '/favicon.svg', type: 'image/svg+xml' },
-      { url: '/favicon.ico', rel: 'shortcut icon' },
+      { url: '/favicon-96x96.png', sizes: '96x96', type: 'image/png' },
     ],
-    apple: [
-      { url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' },
-    ],
+    apple: '/apple-touch-icon.png',
   },
+  manifest: '/site.webmanifest',
 }
 export default function RootLayout({
   children,
