65bbf288cb8a9388a8e8d5c977a11c3a1a180680
tittymaps <info@tittymaps.com>
2026-06-21 12:12:50 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,29 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'ssruvoxuwlksmbmubcfv.supabase.co',
+        pathname: '/storage/v1/object/public/**',
+      },
+    ],
+    minimumCacheTTL: 31536000,
+  },
+  async headers() {
+    return [
+      {
+        source: '/:all*(svg|jpg|jpeg|png|webp|avif|gif|ico)',
+        locale: false,
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          },
+        ],
+      },
+    ]
+  },
+};
 
 module.exports = nextConfig;
