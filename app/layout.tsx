f19148d0b98978941dd6615d76b3b8483f113f46
tittymaps <info@tittymaps.com>
2026-06-30 17:28:15 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export const metadata: Metadata = {
     siteName: 'TittyMaps',
     images: [
       {
-        url: 'https://tittymaps.com/logo-pins.png',
+        url: 'https://tittymaps.com/android-chrome-512x512.png',
         width: 512,
         height: 512,
         alt: 'TittyMaps',
@@ -33,8 +33,10 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       { url: '/favicon.ico', sizes: 'any' },
-      { url: '/favicon.svg', type: 'image/svg+xml' },
-      { url: '/favicon-96x96.png', sizes: '96x96', type: 'image/png' },
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/android-chrome-192x192.png', sizes: '192x192', type: 'image/png' },
+      { url: '/android-chrome-512x512.png', sizes: '512x512', type: 'image/png' },
     ],
     apple: { url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' },
     shortcut: '/favicon.ico',
