bef7055b2b56a3abd70027d83f740e70891a775a
tittymaps <info@tittymaps.com>
2026-06-30 20:36:38 -0400

    Update page.tsx

--- app/states/[state]/page.tsx
+++ app/states/[state]/page.tsx
@@ -4,6 +4,8 @@ import { createClient } from '@supabase/supabase-js'
 import { useParams, useRouter } from 'next/navigation'  // ← added useRouter
 import ProfileButton from '../../components/ProfileButton'
 
+export const revalidate = 0
+
 const supabase = createClient(
   'https://ssruvoxuwlksmbmubcfv.supabase.co',
   'sb_publishable_HpBo6b0DnC-J1B9LL0u26Q_wkkAIAEl'
